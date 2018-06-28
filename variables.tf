81c057efc7e8660ff0fe581dd3b9c3aec79222b2
Sarkis Varozian <svarozian@gmail.com>
2018-06-28 08:44:51 -0700

    parameterize period and evaluation periods

--- variables.tf
+++ variables.tf
@@ -8,14 +8,26 @@ variable "service_name" {
   type        = "string"
 }
 
+variable "evaluation_periods" {
+  description = "Number of periods to evaluate for the alarm."
+  type        = "string"
+  default     = "1"
+}
+
+variable "period" {
+  description = "Duration in seconds to evaluate for the alarm."
+  type        = "string"
+  default     = "300"
+}
+
 variable "cpu_utilization_threshold" {
   description = "The maximum percentage of CPU utilization average."
   type        = "string"
-  default     = 80
+  default     = "80"
 }
 
 variable "memory_utilization_threshold" {
   description = "The maximum percentage of Memory utilization average."
   type        = "string"
-  default     = 80
+  default     = "80"
 }
