487c16abc142ec6ad24da8501357dadfd61c39c2
Sarkis Varozian <svarozian@gmail.com>
2018-06-29 09:31:22 -0700

    allow user defined sns topics

--- main.tf
+++ main.tf
@@ -1,23 +1,41 @@
 data "aws_caller_identity" "default" {}
 
-locals {
-  enabled = "${var.enabled == "true" ? 1 : 0}"
+module "sns_topic_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
+  name       = "${var.name}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  attributes = "${compact(concat(var.attributes, list("alarms")))}"
 }
 
-# Make a topic
+locals {
+  enabled          = "${var.enabled == "true" ? 1 : 0}"
+  create_sns_topic = "${var.create_sns_topic == "true" ? 1 : 0}"
+}
+
+data "aws_sns_topic" "default" {
+  count = "${(1 - local.create_sns_topic) * local.enabled}"
+  name  = "${var.sns_topic_name}"
+}
+
+# Create an SNS topic if one is not passed
 resource "aws_sns_topic" "default" {
-  count       = "${local.enabled}"
-  name_prefix = "ecs-service-threshold-alerts"
+  count       = "${local.enabled * local.create_sns_topic}"
+  name_prefix = "${module.sns_topic_label.id}"
+}
+
+locals {
+  sns_topic_arn = "${element(compact(concat(aws_sns_topic.default.*.arn, data.aws_sns_topic.default.*.arn, list(""))), 0)}"
 }
 
 resource "aws_sns_topic_policy" "default" {
-  count  = "${local.enabled}"
-  arn    = "${aws_sns_topic.default.arn}"
+  count  = "${local.enabled * local.create_sns_topic}"
+  arn    = "${local.sns_topic_arn}"
   policy = "${data.aws_iam_policy_document.sns_topic_policy.json}"
 }
 
 data "aws_iam_policy_document" "sns_topic_policy" {
-  count = "${local.enabled}"
+  count = "${local.enabled * local.create_sns_topic}"
 
   statement {
     actions = [
@@ -33,7 +51,7 @@ data "aws_iam_policy_document" "sns_topic_policy" {
     ]
 
     effect    = "Allow"
-    resources = ["${aws_sns_topic.default.arn}"]
+    resources = ["${local.sns_topic_arn}"]
 
     principals {
       type        = "AWS"
@@ -53,7 +71,7 @@ data "aws_iam_policy_document" "sns_topic_policy" {
   statement {
     sid       = "Allow CloudwatchEvents"
     actions   = ["sns:Publish"]
-    resources = ["${aws_sns_topic.default.arn}"]
+    resources = ["${local.sns_topic_arn}"]
 
     principals {
       type        = "Service"
