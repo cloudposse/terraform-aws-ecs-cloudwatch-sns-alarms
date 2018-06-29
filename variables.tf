1ee04613f87b44968740c59896a20a3cad005ff6
Sarkis Varozian <svarozian@gmail.com>
2018-06-29 12:32:32 -0700

    allow user defined sns topics (#2)

--- variables.tf
+++ variables.tf
@@ -37,6 +37,24 @@ variable "enabled" {
   default     = "true"
 }
 
+variable "create_sns_topic" {
+  type        = "string"
+  description = "Determines if a new sns topic will be generated. If set to false, the existing sns_topic_name variable must be set."
+  default     = "true"
+}
+
+variable "sns_topic_name" {
+  type        = "string"
+  description = "Name of existing SNS topic to use for alarm and ok actions, instead of generating a new one."
+  default     = ""
+}
+
+variable "additional_notify_arns" {
+  type        = "list"
+  description = "Optional list of additional ARNs to notify on alarm and ok actions."
+  default     = []
+}
+
 variable "cluster_name" {
   type        = "string"
   description = "The name of the ECS cluster to monitor."
