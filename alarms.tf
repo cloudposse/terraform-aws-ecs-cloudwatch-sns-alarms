32136ea23f895763f3a269d2f3c6db08cf98bc0f
Sarkis Varozian <svarozian@gmail.com>
2018-07-05 23:55:06 -0700

    Refactor module to support autoscaling ARNs

--- alarms.tf
+++ alarms.tf
@@ -1,23 +1,41 @@
-module "cpu_alarm_label" {
+module "cpu_utilization_high_alarm_label" {
   source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
   name       = "${var.name}"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
-  attributes = "${compact(concat(var.attributes, list("cpu", "util", "high")))}"
+  attributes = "${compact(concat(var.attributes, list("cpu", "utilization", "high")))}"
 }
 
-module "memory_alarm_label" {
+module "cpu_utilization_low_alarm_label" {
   source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
   name       = "${var.name}"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
-  attributes = "${compact(concat(var.attributes, list("mem", "util", "high")))}"
+  attributes = "${compact(concat(var.attributes, list("cpu", "utilization", "low")))}"
+}
+
+module "memory_utilization_high_alarm_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
+  name       = "${var.name}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  attributes = "${compact(concat(var.attributes, list("memory", "utilization", "high")))}"
+}
+
+module "memory_utilization_low_alarm_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
+  name       = "${var.name}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  attributes = "${compact(concat(var.attributes, list("memory", "utilization", "low")))}"
 }
 
 locals {
   thresholds = {
-    CPUUtilizationThreshold    = "${min(max(var.cpu_utilization_threshold, 0), 100)}"
-    MemoryUtilizationThreshold = "${min(max(var.memory_utilization_threshold, 0), 100)}"
+    CPUUtilizationHighThreshold    = "${min(max(var.cpu_utilization_high_threshold, 0), 100)}"
+    CPUUtilizationLowThreshold     = "${min(max(var.cpu_utilization_low_threshold, 0), 100)}"
+    MemoryUtilizationHighThreshold = "${min(max(var.memory_utilization_high_threshold, 0), 100)}"
+    MemoryUtilizationLowThreshold  = "${min(max(var.memory_utilization_low_threshold, 0), 100)}"
   }
 
   dimensions_map = {
@@ -32,36 +50,70 @@ locals {
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "cpu_utilization_too_high" {
+resource "aws_cloudwatch_metric_alarm" "cpu_utilization_high" {
   count               = "${local.enabled}"
-  alarm_name          = "${module.cpu_alarm_label.id}"
+  alarm_name          = "${module.cpu_utilization_high_alarm_label}"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "${var.evaluation_periods}"
+  evaluation_periods  = "${var.cpu_utilization_high_evaluation_periods}"
   metric_name         = "CPUUtilization"
   namespace           = "AWS/ECS"
-  period              = "${var.period}"
+  period              = "${var.cpu_utilization_high_period}"
   statistic           = "Average"
-  threshold           = "${local.thresholds["CPUUtilizationThreshold"]}"
-  alarm_description   = "${format(var.alarm_description, "CPU", var.period/60, var.evaluation_periods)}"
-  alarm_actions       = ["${var.notify_arns}"]
-  ok_actions          = ["${var.notify_arns}"]
+  threshold           = "${local.thresholds["CPUUtilizationHighThreshold"]}"
+  alarm_description   = "${format(var.alarm_description, "CPU", "High", var.cpu_utilization_high_period/60, var.cpu_utilization_high_evaluation_periods)}"
+  alarm_actions       = ["${compact(var.cpu_utilization_high_alarm_actions)}"]
+  ok_actions          = ["${compact(var.cpu_utilization_high_ok_actions)}"]
 
   dimensions = "${local.dimensions_map[var.service_name == "" ? "cluster" : "service"]}"
 }
 
-resource "aws_cloudwatch_metric_alarm" "memory_utilization_too_high" {
+resource "aws_cloudwatch_metric_alarm" "cpu_utilization_low" {
   count               = "${local.enabled}"
-  alarm_name          = "${module.memory_alarm_label.id}"
+  alarm_name          = "${module.cpu_utilization_low_alarm_label.id}"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "${var.cpu_utilization_low_evaluation_periods}"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "${var.cpu_utilization_low_period}"
+  statistic           = "Average"
+  threshold           = "${local.thresholds["CPUUtilizationLowThreshold"]}"
+  alarm_description   = "${format(var.alarm_description, "CPU", "Low", var.cpu_utilization_low_period/60, var.cpu_utilization_low_evaluation_periods)}"
+  alarm_actions       = ["${compact(var.cpu_utilization_low_alarm_actions)}"]
+  ok_actions          = ["${compact(var.cpu_utilization_low_ok_actions)}"]
+
+  dimensions = "${local.dimensions_map[var.service_name == "" ? "cluster" : "service"]}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "memory_utilization_high" {
+  count               = "${local.enabled}"
+  alarm_name          = "${module.memory_utilization_high_alarm_label}"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "${var.evaluation_periods}"
+  evaluation_periods  = "${var.memory_utilization_high_evaluation_periods}"
   metric_name         = "MemoryUtilization"
   namespace           = "AWS/ECS"
-  period              = "${var.period}"
+  period              = "${var.memory_utilization_high_period}"
   statistic           = "Average"
-  threshold           = "${local.thresholds["MemoryUtilizationThreshold"]}"
-  alarm_description   = "${format(var.alarm_description, "Memory", var.period/60, var.evaluation_periods)}"
-  alarm_actions       = ["${var.notify_arns}"]
-  ok_actions          = ["${var.notify_arns}"]
+  threshold           = "${local.thresholds["MemoryUtilizationHighThreshold"]}"
+  alarm_description   = "${format(var.alarm_description, "Memory", "Hight", var.memory_utilization_high_period/60, var.memory_utilization_high_evaluation_periods)}"
+  alarm_actions       = ["${compact(var.memory_utilization_high_alarm_actions)}"]
+  ok_actions          = ["${compact(var.memory_utilization_high_ok_actions)}"]
+
+  dimensions = "${local.dimensions_map[var.service_name == "" ? "cluster" : "service"]}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "memory_utilization_low" {
+  count               = "${local.enabled}"
+  alarm_name          = "${module.memory_utilization_low_alarm_label}"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "${var.memory_utilization_low_evaluation_periods}"
+  metric_name         = "MemoryUtilization"
+  namespace           = "AWS/ECS"
+  period              = "${var.memory_utilization_low_period}"
+  statistic           = "Average"
+  threshold           = "${local.thresholds["MemoryUtilizationLowThreshold"]}"
+  alarm_description   = "${format(var.alarm_description, "Memory", "Low", var.memory_utilization_low_period/60, var.memory_utilization_low_evaluation_periods)}"
+  alarm_actions       = ["${compact(var.memory_utilization_low_alarm_actions)}"]
+  ok_actions          = ["${compact(var.memory_utilization_low_ok_actions)}"]
 
   dimensions = "${local.dimensions_map[var.service_name == "" ? "cluster" : "service"]}"
 }
