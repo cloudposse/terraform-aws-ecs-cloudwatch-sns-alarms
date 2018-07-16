e890a95e35b046b09c4676117ad51d17b5764eee
Sarkis Varozian <42673+sarkis@users.noreply.github.com>
2018-07-16 13:13:37 -0700

    Refactor module to support autoscaling (#4)
    * Refactor module to support autoscaling ARNs

--- docs/terraform.md
+++ docs/terraform.md
@@ -3,18 +3,33 @@
 
 | Name | Description | Type | Default | Required |
 |------|-------------|:----:|:-----:|:-----:|
-| alarm_description | The string to format and use as the alarm description. | string | `Average service %v utilization over last %d minute(s) too high over %v period(s)` | no |
+| alarm_description | The string to format and use as the alarm description. | string | `Average service %v utilization %v last %d minute(s) over %v period(s)` | no |
 | attributes | List of attributes to add to label. | list | `<list>` | no |
 | cluster_name | The name of the ECS cluster to monitor. | string | - | yes |
-| cpu_utilization_threshold | The maximum percentage of CPU utilization average. | string | `80` | no |
+| cpu_utilization_high_alarm_actions | A list of ARNs (i.e. SNS Topic ARN) to notify on CPU Utilization High Alarm action. | list | `<list>` | no |
+| cpu_utilization_high_evaluation_periods | Number of periods to evaluate for the alarm. | string | `1` | no |
+| cpu_utilization_high_ok_actions | A list of ARNs (i.e. SNS Topic ARN) to notify on CPU Utilization High OK action. | list | `<list>` | no |
+| cpu_utilization_high_period | Duration in seconds to evaluate for the alarm. | string | `300` | no |
+| cpu_utilization_high_threshold | The maximum percentage of CPU utilization average. | string | `80` | no |
+| cpu_utilization_low_alarm_actions | A list of ARNs (i.e. SNS Topic ARN) to notify on CPU Utilization Low Alarm action. | list | `<list>` | no |
+| cpu_utilization_low_evaluation_periods | Number of periods to evaluate for the alarm. | string | `1` | no |
+| cpu_utilization_low_ok_actions | A list of ARNs (i.e. SNS Topic ARN) to notify on CPU Utilization Low OK action. | list | `<list>` | no |
+| cpu_utilization_low_period | Duration in seconds to evaluate for the alarm. | string | `300` | no |
+| cpu_utilization_low_threshold | The minimum percentage of CPU utilization average. | string | `20` | no |
 | delimiter | The delimiter to be used in labels. | string | `-` | no |
 | enabled | Whether to create all resources | string | `true` | no |
-| evaluation_periods | Number of periods to evaluate for the alarm. | string | `1` | no |
-| memory_utilization_threshold | The maximum percentage of Memory utilization average. | string | `80` | no |
+| memory_utilization_high_alarm_actions | A list of ARNs (i.e. SNS Topic ARN) to notify on Memory Utilization High Alarm action. | list | `<list>` | no |
+| memory_utilization_high_evaluation_periods | Number of periods to evaluate for the alarm. | string | `1` | no |
+| memory_utilization_high_ok_actions | A list of ARNs (i.e. SNS Topic ARN) to notify on Memory Utilization High OK action. | list | `<list>` | no |
+| memory_utilization_high_period | Duration in seconds to evaluate for the alarm. | string | `300` | no |
+| memory_utilization_high_threshold | The maximum percentage of Memory utilization average. | string | `80` | no |
+| memory_utilization_low_alarm_actions | A list of ARNs (i.e. SNS Topic ARN) to notify on Memory Utilization Low Alarm action. | list | `<list>` | no |
+| memory_utilization_low_evaluation_periods | Number of periods to evaluate for the alarm. | string | `1` | no |
+| memory_utilization_low_ok_actions | A list of ARNs (i.e. SNS Topic ARN) to notify on Memory Utilization Low OK action. | list | `<list>` | no |
+| memory_utilization_low_period | Duration in seconds to evaluate for the alarm. | string | `300` | no |
+| memory_utilization_low_threshold | The minimum percentage of Memory utilization average. | string | `20` | no |
 | name | Name (unique identifier for app or service) | string | - | yes |
 | namespace | Namespace (e.g. `cp` or `cloudposse`) | string | - | yes |
-| notify_arns | A list of ARNs (i.e. SNS Topic ARN) to notify on alarm and ok actions. | list | - | yes |
-| period | Duration in seconds to evaluate for the alarm. | string | `300` | no |
 | service_name | The name of the ECS Service in the ECS cluster to monitor. | string | `` | no |
 | stage | Stage (e.g. `prod`, `dev`, `staging`) | string | - | yes |
 | tags | Map of key-value pairs to use for tags. | map | `<map>` | no |
--- variables.tf
+++ variables.tf
@@ -37,11 +37,6 @@ variable "enabled" {
   default     = "true"
 }
 
-variable "notify_arns" {
-  type        = "list"
-  description = "A list of ARNs (i.e. SNS Topic ARN) to notify on alarm and ok actions."
-}
-
 variable "cluster_name" {
   type        = "string"
   description = "The name of the ECS cluster to monitor."
@@ -53,32 +48,128 @@ variable "service_name" {
   default     = ""
 }
 
-variable "evaluation_periods" {
+variable "alarm_description" {
   type        = "string"
-  description = "Number of periods to evaluate for the alarm."
-  default     = "1"
+  description = "The string to format and use as the alarm description."
+  default     = "Average service %v utilization %v last %d minute(s) over %v period(s)"
 }
 
-variable "period" {
-  type        = "string"
-  description = "Duration in seconds to evaluate for the alarm."
-  default     = "300"
-}
-
-variable "cpu_utilization_threshold" {
+variable "cpu_utilization_high_threshold" {
   type        = "string"
   description = "The maximum percentage of CPU utilization average."
   default     = "80"
 }
 
-variable "memory_utilization_threshold" {
+variable "cpu_utilization_high_evaluation_periods" {
+  type        = "string"
+  description = "Number of periods to evaluate for the alarm."
+  default     = "1"
+}
+
+variable "cpu_utilization_high_period" {
+  type        = "string"
+  description = "Duration in seconds to evaluate for the alarm."
+  default     = "300"
+}
+
+variable "cpu_utilization_high_alarm_actions" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to notify on CPU Utilization High Alarm action."
+  default     = []
+}
+
+variable "cpu_utilization_high_ok_actions" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to notify on CPU Utilization High OK action."
+  default     = []
+}
+
+variable "cpu_utilization_low_threshold" {
+  type        = "string"
+  description = "The minimum percentage of CPU utilization average."
+  default     = "20"
+}
+
+variable "cpu_utilization_low_evaluation_periods" {
+  type        = "string"
+  description = "Number of periods to evaluate for the alarm."
+  default     = "1"
+}
+
+variable "cpu_utilization_low_period" {
+  type        = "string"
+  description = "Duration in seconds to evaluate for the alarm."
+  default     = "300"
+}
+
+variable "cpu_utilization_low_alarm_actions" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to notify on CPU Utilization Low Alarm action."
+  default     = []
+}
+
+variable "cpu_utilization_low_ok_actions" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to notify on CPU Utilization Low OK action."
+  default     = []
+}
+
+variable "memory_utilization_high_threshold" {
   type        = "string"
   description = "The maximum percentage of Memory utilization average."
   default     = "80"
 }
 
-variable "alarm_description" {
+variable "memory_utilization_high_evaluation_periods" {
   type        = "string"
-  description = "The string to format and use as the alarm description."
-  default     = "Average service %v utilization over last %d minute(s) too high over %v period(s)"
+  description = "Number of periods to evaluate for the alarm."
+  default     = "1"
+}
+
+variable "memory_utilization_high_period" {
+  type        = "string"
+  description = "Duration in seconds to evaluate for the alarm."
+  default     = "300"
+}
+
+variable "memory_utilization_high_alarm_actions" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to notify on Memory Utilization High Alarm action."
+  default     = []
+}
+
+variable "memory_utilization_high_ok_actions" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to notify on Memory Utilization High OK action."
+  default     = []
+}
+
+variable "memory_utilization_low_threshold" {
+  type        = "string"
+  description = "The minimum percentage of Memory utilization average."
+  default     = "20"
+}
+
+variable "memory_utilization_low_evaluation_periods" {
+  type        = "string"
+  description = "Number of periods to evaluate for the alarm."
+  default     = "1"
+}
+
+variable "memory_utilization_low_period" {
+  type        = "string"
+  description = "Duration in seconds to evaluate for the alarm."
+  default     = "300"
+}
+
+variable "memory_utilization_low_alarm_actions" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to notify on Memory Utilization Low Alarm action."
+  default     = []
+}
+
+variable "memory_utilization_low_ok_actions" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to notify on Memory Utilization Low OK action."
+  default     = []
 }
