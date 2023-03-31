4c0b390e89f410d3a5e6157e34508bc1dca1b525
nitrocode <7775707+nitrocode@users.noreply.github.com>
2023-03-31 18:41:10 -0500

    Use cloudposse/template for arm support (#34)
    * Use cloudposse/template for arm support
    
    * Auto Format
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -6,8 +6,8 @@ terraform {
       version = ">= 2.0"
     }
     template = {
-      source  = "hashicorp/template"
-      version = ">= 2.0"
+      source  = "cloudposse/template"
+      version = ">= 2.2"
     }
     local = {
       source  = "hashicorp/local"
