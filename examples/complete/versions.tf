c08347ae9c59dcd7a03a3d7a71e85bb86bb578d1
Joe Niland <joe@originalmind.com.au>
2024-03-04 19:37:33 +1100

    Update upstream module versions in examples/complete (#41)
    * examples/complete - bump upstream module versions
    
    - bump min provider versions in line with deps
    
    * Update docs
    
    * docs: Fix provider pinning
    
    * fix vpc/subnet var names
    
    * fix vpc/subnet var types

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = ">= 4.4.0"
     }
     template = {
       source  = "cloudposse/template"
